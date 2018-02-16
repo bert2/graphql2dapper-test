6b4fb8b7d56b4ea09a0070e6de045934461f3804
bert2 <shuairan@gmail.com>
2018-02-16 19:44:27 +0100

    Add db context.

---
+++ Example/ExampleContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Example
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Models;
+
+    public class ExampleContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<Address> Addresses { get; set; }
+
+        public DbSet<City> Cities { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=example.db");
+        }
+    }
+}
