74a858edfe95edf7cb7db28e9f70fcfe6fcf99a9
bert2 <shuairan@gmail.com>
2018-02-16 19:54:16 +0100

    Add example executable.

---
+++ Example.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Example.Cli
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
